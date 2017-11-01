55260f66fe2a85fa73219beb20cf0ad6b8e6babf
Jim Eli <31859133+JimEli@users.noreply.github.com>
2017-11-01 11:23:36 -0600

    Update stack.c

--- GenericStack/stack.c
+++ GenericStack/stack.c
@@ -1,75 +1,79 @@
 //stack.c
 #include "stack.h"
 
-void stackCreate(stack *s, int elementSize) {
-	assert(elementSize > 0);
+bool stackCreate(Stack *stack, int elementSize) {
+	//assert(elementSize > 0);
+	if (elementSize > 0) {
+		stack->elementSize = elementSize;
+		stack->logLength = 0;
+		stack->allocLength = kInitialAllocationSize;
+		stack->elements = malloc(kInitialAllocationSize * elementSize);
 
-	s->elementSize = elementSize;
-	s->logLength = 0;
-	s->allocLength = kInitialAllocationSize;
-	s->elements = malloc(kInitialAllocationSize * elementSize);
-	
-	assert(s->elements != NULL);
+		//assert(stack->elements != NULL);
+		if (stack->elements != NULL)
+			return true;
+	}
+	return false;
 }
 
-void stackDestroy(stack *s) {
-	free(s->elements);
+void stackDestroy(Stack *stack) {
+	free(stack->elements);
 }
 
-bool stackEmpty(const stack *s) {
-	return s->logLength == 0;
+bool stackEmpty(const Stack *stack) {
+	return stack->logLength == 0;
 }
 
 // Push dynamically reallocates the array if needed to be larger.
-bool stackPush(stack *s, const void *elementAddr) {
-	if (s->logLength == s->allocLength) {
-		s->allocLength *= 2;
-		s->elements = realloc(s->elements, s->allocLength * s->elementSize);
-		//assert(s->elems != NULL);
-		if (s->elements == NULL)
+bool stackPush(Stack *stack, const void *elementAddr) {
+	if (stack->logLength == stack->allocLength) {
+		stack->allocLength *= 2;
+		stack->elements = realloc(stack->elements, stack->allocLength * stack->elementSize);
+		//assert(stack->elems != NULL);
+		if (stack->elements == NULL)
 			return false;
 	}
-	void *destAddr = (char *)s->elements + s->logLength * s->elementSize;
-	memcpy(destAddr, elementAddr, s->elementSize);
-	s->logLength++;
+	void *destAddr = (char *)stack->elements + stack->logLength * stack->elementSize;
+	memcpy(destAddr, elementAddr, stack->elementSize);
+	stack->logLength++;
 	return true;
 }
 
 // Pop never reallocates the array to be smaller.
-bool stackPop(stack *s, void *elementAddr) {
-	//assert(!stackEmpty(s));
-	if (!stackEmpty(s)) {
-		s->logLength--;
-		const void *sourceAddr = (const char *)s->elements + s->logLength * s->elementSize;
-		memcpy(elementAddr, sourceAddr, s->elementSize);
+bool stackPop(Stack *stack, void *elementAddr) {
+	//assert(!stackEmpty(stack));
+	if (!stackEmpty(stack)) {
+		stack->logLength--;
+		const void *sourceAddr = (const char *)stack->elements + stack->logLength * stack->elementSize;
+		memcpy(elementAddr, sourceAddr, stack->elementSize);
 		return true;
 	}
 	return false;
 }
 
-bool stackPeek(stack *s, void *elementAddr) {
-	//assert(!stackEmpty(s));
-	if (!stackEmpty(s)) {
-		const void *sourceAddr = (const char *)s->elements + (s->logLength - 1) * s->elementSize;
-		memcpy(elementAddr, sourceAddr, s->elementSize);
+bool stackPeek(Stack *stack, void *elementAddr) {
+	//assert(!stackEmpty(stack));
+	if (!stackEmpty(stack)) {
+		const void *sourceAddr = (const char *)stack->elements + (stack->logLength - 1) * stack->elementSize;
+		memcpy(elementAddr, sourceAddr, stack->elementSize);
 		return true;
 	}
 	return false;
 }
 
-void stackPrint(stack *s, void(*print)(int *), void *elementAddr) {
-	if (!stackEmpty(s)) {
-		for (int i = 0; i < s->logLength; i++) {
-			const void *sourceAddr = (const char *)s->elements + i * s->elementSize;
-			memcpy(elementAddr, sourceAddr, s->elementSize);
+void stackPrint(Stack *stack, void(*print)(int *), void *elementAddr) {
+	if (!stackEmpty(stack)) {
+		for (int i = 0; i < stack->logLength; i++) {
+			const void *sourceAddr = (const char *)stack->elements + i * stack->elementSize;
+			memcpy(elementAddr, sourceAddr, stack->elementSize);
 			(* print)(elementAddr);
 		}
 		puts("");
 	}
 }
 
-int stackSize(stack *s) {
-	if (s)
-		return s->logLength;
+int stackSize(Stack *stack) {
+	if (stack)
+		return stack->logLength;
 	return 0;
 }
